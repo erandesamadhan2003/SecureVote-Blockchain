0929c694275afa0cb59e686a836283f8c9adb2ac
Sakshi Kumari <sn125798@gmail..com>
2025-08-23 22:22:29 +0530

    voterregistory

--- Blockchain/contracts/VoterRegistry.sol
+++ Blockchain/contracts/VoterRegistry.sol
@@ -1,132 +1,100 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+  
 contract VoterRegistry {
-    // enum
-    enum VerificationStatus { Pending, UnderReview, Verified, Rejected }
-
+   
+    //manager contract reference
+    address public immutable manager;
+    
+  
+    // state variables
+    mapping(address => Voter) public voters;
+    
     // structs
     struct Voter {
         address walletAddress;
-        bytes32 aadharHash;
-        string encryptedPersonalinfo;
-        VerificationStatus kycStatus;
-        bool isEligible;
+        string name;
+        string aadharHash;
+        bool isVerified;
         bool hasVoted;
-        uint256 registrationTime;
-        address verifiedBy;
     }
 
-    // state variables
-    mapping(address => Voter) public voters;
-    mapping(bytes32 => address) private aadhaarToAddress;
-    mapping(address => bool) private registeredVoters;
-    address[] public voterList;
-    uint256 public totalRegistered;
-    uint256 public totalVerified;
+    //MODIFIERS
+    modifier onlyManager(){
+        require(isManagerElectionManager(msg.sender),"Not election manager");
+        _;
+    }
+    
 
     // Events
-    event VoterRegistered(address indexed voter, uint256 timestamp);
-    event VoterVerified(address indexed voter, address verifier);
+    event VoterRegistered(address  voter, string name,string aadharHash);
+    event VoterVerified(address  voter);
+    event VoterVoted(address voter);
+    
 
-    // --- Query Functions ---
-    // check if voter registered or not
-    function isRegistered(address _voter) public view returns (bool) {
-        return registeredVoters[_voter];
+    //constructor
+    constructor(address _manager) {
+          manager = _manager;
+        }
+
+
+    //check manager electionManager role
+    function isManagerElectionManager(address account) public view returns(bool){
+        (bool success, bytes memory result)=manager.staticcall(
+           abi.encodeWithSignature(
+            "hasRole(bytes32,address)",
+             keccak256("ELECTION_MANAGER"),
+             account
+           )
+        );
+        return success && abi.decode(result,(bool));
+
     }
+
+
+    //function for  voter Registration
+    function registerVoter(string memory _name,string memory _aadharHash) public {
+        require(voters[msg.sender].walletAddress==address(0),"voter already registered");
+        voters[msg.sender]=Voter({
+            walletAddress:msg.sender,
+            name:_name,
+            aadharHash:_aadharHash,
+            isVerified:false,
+            hasVoted:false
+        });
+
+        emit VoterRegistered(msg.sender,_name,_aadharHash);
+
+    }
+    
+    // function for verifyvoter
+    function verifyVoter(address _voter) public onlyManager{
+        require(voters[_voter].walletAddress!=address(0),"voter is not registered yet");
+        voters[_voter].isVerified=true;
+        emit VoterVerified(_voter);
+    }
+
 
     // check if voter is eligible
     function isEligible(address _voter) public view returns (bool) {
-        return voters[_voter].isEligible;
+        require(voters[_voter].isVerified==true,"voter is not verified");
+        require(voters[_voter].hasVoted==false,"voter already give thier vote");
+        return true;
     }
 
-    // check if voter has voted
-    function hasVoted(address _voter) public view returns (bool) {
-        return voters[_voter].hasVoted;
-    }
 
-    // get complete voter information
-    function getVoterInfo(address _voter) external view returns (Voter memory) {
-        return voters[_voter];
-    }
+    // function for mark vote
+    function markVoted(address _voter)  public onlyManager{
+        require(voters[_voter].isVerified==true,"voter is not verified");
+        require(voters[_voter].hasVoted==false,"voter alredy give thier vote");
+        voters[_voter].hasVoted=true;
+        emit VoterVoted(_voter);
+    } 
 
-    // --- Registration Function ---
-    function registerVoter(
-        bytes32 _aadhaarHash,
-        string memory _encryptedPersonalInfo
-    ) external {
-        require(!isRegistered(msg.sender), "Already registered");
-        require(
-            aadhaarToAddress[_aadhaarHash] == address(0),
-            "Aadhaar already used"
-        );
 
-        // create voter
-        voters[msg.sender] = Voter({
-            walletAddress: msg.sender,
-            aadharHash: _aadhaarHash,
-            encryptedPersonalinfo: _encryptedPersonalInfo,
-            kycStatus: VerificationStatus.Pending,
-            isEligible: false,
-            hasVoted: false,
-            registrationTime: block.timestamp,
-            verifiedBy: address(0)
-        });
 
-        // mark as registered
-        registeredVoters[msg.sender] = true;
+    
 
-        // store mapping for duplicate prevention
-        aadhaarToAddress[_aadhaarHash] = msg.sender;
-
-        voterList.push(msg.sender);
-        totalRegistered++;
-
-        emit VoterRegistered(msg.sender, block.timestamp);
-    }
-
-    // --- Verification Function ---
-    modifier onlyVerifier() {
-        // TODO: Add verifier logic (role-based access control)
-        _;
-    }
-
-    function verifyVoter(address _voter) external onlyVerifier {
-        require(isRegistered(_voter), "Voter not registered");
-        require(
-            voters[_voter].kycStatus == VerificationStatus.Pending,
-            "Not pending"
-        );
-
-        voters[_voter].kycStatus = VerificationStatus.Verified;
-        voters[_voter].isEligible = true;
-        voters[_voter].verifiedBy = msg.sender;
-        totalVerified++;
-
-        emit VoterVerified(_voter, msg.sender);
-    }
-
-    // --- Reject Voter ---
-    function rejectVoter(
-        address _voter,
-        string memory _reason
-    ) external onlyVerifier {
-        require(isRegistered(_voter), "Voter not registered");
-        voters[_voter].kycStatus = VerificationStatus.Rejected;
-
-        // TODO: Store/log rejected reason if needed
-    }
-
-    // --- Voting Status Management ---
-    modifier onlyVotingEngine() {
-        // TODO: Add logic for voting engine role
-        _;
-    }
-
-    function markAsVoted(address _voter) external onlyVotingEngine {
-        require(isEligible(_voter), "Not eligible");
-        require(!voters[_voter].hasVoted, "Already voted");
-
-        voters[_voter].hasVoted = true;
-    }
 }
