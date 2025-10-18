ee30e9eb8070ea56ec336bac64cd18fe6b43acaa
erandesamadhan2003 <erandesamadhan2003@gmail.com>
2025-10-19 00:28:16 +0530

    Frontend backend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container mx-auto p-4 text-2xl'>
+      <h1>Welcome to the Frontend Application</h1>
+    </div>
+  )
+}
+
+export default App
